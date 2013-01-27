b0e6036220f244c9b403b6d7728633206dbfb8eb
iclanzan <iclanzan@macbook>
2013-01-27 02:12:19 +0200

    Fixed gapi not being defined.

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
 (function(root, undefined) {
   'use strict';
 
-  var gapi = root.gapi,
-      clientID = '589507031071.apps.googleusercontent.com',
+  var clientID = '589507031071.apps.googleusercontent.com',
       scope = 'https://www.googleapis.com/auth/drive';
 
   function auth(result) {
@@ -10,11 +9,11 @@
       console.log('Authorized');
     }
     else
-      gapi.auth.authorize({'client_id': clientID, 'scope': scope, 'immediate': false}, auth);
+      root.gapi.auth.authorize({'client_id': clientID, 'scope': scope, 'immediate': false}, auth);
   }
 
   root.app = function() {
-    gapi.auth.authorize({'client_id': clientID, 'scope': scope, 'immediate': true}, auth);
+    root.gapi.auth.authorize({'client_id': clientID, 'scope': scope, 'immediate': true}, auth);
   };
 
 })(this);
