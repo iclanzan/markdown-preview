bb86cc2b49c0ccf410d79648f8281956f09af790
iclanzan <iclanzan@macbook>
2013-01-30 15:07:09 +0200

    Integrated analytics.js

--- app.js
+++ app.js
@@ -4,7 +4,24 @@
   var clientID = '589507031071.apps.googleusercontent.com',
       scope = 'https://www.googleapis.com/auth/drive.file https://www.googleapis.com/auth/drive.install',
       doc = root.document,
+      analytics = root.analytics || [],
       marked, hljs, gapi, article;
+
+  // Load analytics
+  require('d2dq2ahtl5zl1z.cloudfront.net/analytics.js/v1/892gioqtse/analytics.min.js');
+  analytics.push(['pageview']);
+
+  function require(url, callback) {
+    var script = doc.createElement('script');
+    script.async = true;
+    script.src = url;
+
+    if (callback) {
+      script.onload = callback;
+    }
+
+    doc.body.appendChild(script);
+  }
 
   function getParam(name) {
     var results = new RegExp('[\\?&]' + name.replace(/[\[]/, '\\\[').replace(/[\]]/, '\\\]') + '=([^&#]*)')
@@ -19,12 +36,16 @@
 
   function loadFile() {
     var state = JSON.parse(getParam('state') || '""'),
-        client = gapi.client;
+        client = gapi.client,
+        fileId;
     if (!state || !state.ids || !state.ids.length) return;
+    fileId = state.ids[0];
+
+    analytics.push(['identify', state.userId], ['track', 'opened file', {fileId: fileId}]);
 
     client.load('drive', 'v2', function() {
       client.drive.files.get({
-        'fileId': state.ids[0]
+        'fileId': fileId
       }).execute(function(fileMeta) {
         var url = fileMeta.downloadUrl;
         if (url) {
@@ -53,6 +74,7 @@
     else {
       article.innerHTML = '<div id="install">Install</div>';
       $('install').onclick = function() {
+        analytics.push(['track', 'clicked on install button']);
         gapi.auth.authorize({'client_id': clientID, 'scope': scope, 'immediate': false}, auth);
       };
     }
